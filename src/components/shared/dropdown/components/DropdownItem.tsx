3c8beacf634a01afca106e97e6788de6d68db93c
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2023-04-20 21:53:08 +0400

    chore: add text for dropdown items

--- src/components/shared/dropdown/components/DropdownItem.tsx
+++ src/components/shared/dropdown/components/DropdownItem.tsx
@@ -1,18 +1,17 @@
 import { arrowDownIcon, arrowUpIcon } from '@/assets'
 import { useState } from 'react'
+import { DropdownItemProps } from './types'
 
-const DropdownItem = () => {
+const DropdownItem: React.FC<DropdownItemProps> = ({ text, children }) => {
   const [textIsShown, setTextIsShown] = useState(false)
 
   return (
     <div>
       <div
-        className='bg-background_black_shade_3 px-[1rem] py-[1.7rem] rounded-[0.8rem] flex items-center justify-between gap-4 cursor-pointer'
+        className='bg-background_black_shade_3 px-[1.4rem] py-[1.7rem] rounded-[0.8rem] flex items-center justify-between gap-4 cursor-pointer'
         onClick={() => setTextIsShown((prevState) => !prevState)}
       >
-        <p className='text-gray_shade_4 leading-7'>
-          როდის იწყება და რა ფორმატით გაიმართება აქცია
-        </p>
+        <p className='text-gray_shade_4 leading-7 md:text-white'>{text}</p>
 
         <img
           src={textIsShown ? arrowUpIcon : arrowDownIcon}
@@ -22,20 +21,9 @@ const DropdownItem = () => {
       </div>
 
       {textIsShown && (
-        <div>
-          <ul className='list-disc list-inside'>
-            <li>
-              აქცია დაიწყება 1 აპრილს 00:00 საათზე და დასრულდება 30 აპრილს 23:59
-              საათზე.
-            </li>
-            <li>
-              აქცია ჩატარდება სამ ეტაპად:
-              <ul className='list-disc list-inside'>
-                <li>Cash Game - 1 აპრილიდან 29 აპრილის ჩათვლით.</li>
-                <li>Spring Series - 13 აპრილიდან 29 აპრილის ჩათვლით.</li>
-                <li>Final Stage – 30 აპრილი.</li>
-              </ul>
-            </li>
+        <div className='mt-[2rem] ml-[2rem] leading-6 text-gray_shade_4 mb-[2rem]'>
+          <ul className='list-disc list-inside marker:text-main_orange space-y-4'>
+            {children}
           </ul>
         </div>
       )}
---
+++ src/components/shared/dropdown/components/types.d.ts
@@ -0,0 +1,4 @@
+export type DropdownItemProps = {
+  children: React.ReactNode
+  text: string
+}
